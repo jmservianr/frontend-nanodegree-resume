535fb7b0c47533d7f94f772eb4bf0d732bf407fd
jmservianr <jmservianr@users.noreply.github.com>
2016-03-09 18:09:14 +0100

    Revert "Primeros ejercicioes"
    This reverts commit 06f9ee4863a02f93b094f63f35d1dfbb7c00e91d.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,24 +1,3 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-
- //$("#main").append ("Jose Maria");
-
-var awesomeThoughts;
-
-awesomeThoughts="I am Jose and I am AWESOME";
-
-var funThoughts=awesomeThoughts.replace("AWESOME","FUN");
-$("#main").append (funThoughts);
-
-var formattedName, formattedRole;
-
-formattedName=HTMLheaderName.replace("%data%","Jose Maria");
-formattedRole=HTMLheaderRole.replace("%data%","Ingeniero Telecomunicaciones");
-
-
-//$("#header").append(formattedRole);
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-console.log("hola");
